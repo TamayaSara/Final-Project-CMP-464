ea477449f01f2717786da2eb4fdafcdb9941be6b
Tamaya Sara <95479134+TamayaSara@users.noreply.github.com>
2023-05-23 18:34:16 -0400

    added errorHandler js file to the server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('epress')
+const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
 
 app.use('/', express.static(path.join(__dirname, 'public')))
@@ -17,5 +19,8 @@ app.all ('*', (req, res)=> {
         res.type('txt').send('404 Not Found')
     }
 } )
+
+app.use(errorHandler)
+
 
 app.listen(PORT, () => console.log('server running on port ${PORT}'))
